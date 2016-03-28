1790d7963ab9074d9af37e7affe2809c8f26e8b5
unknown <viktor.shevchenko@KBP1-LHP-F54608.synapse.com>
2016-03-28 18:30:00 +0300

    filtering box and check all

--- app/components/DevicesList.jsx
+++ app/components/DevicesList.jsx
@@ -15,7 +15,7 @@ class ListItem extends React.Component {
                 <div className="list-item__main-section">
                     <div className="list-item__icon" >
                         <label className="full-size list-item__icon-label">
-                            <input type="checkbox" />
+                            <input type="checkbox" checked={this.props.credential.checked}/>
                         </label>
                     </div>
                     <div className="list-item__partial list-item__name" >
@@ -45,8 +45,25 @@ export default class DevicesList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: deviceCredentials
+            list: deviceCredentials,
+            allChecked: false
         }
+    }
+
+    checkAll (e) {
+        console.log("logging");
+
+       const lst = this.state.list.map(item => {
+           item.checked = !this.state.allChecked;
+           return item;
+       });
+
+        this.setState({
+            allChecked: !this.state.allChecked,
+            list: lst
+        });
+
+        e.stopPropagation();
     }
 
     filerList (item) {
@@ -55,9 +72,21 @@ export default class DevicesList extends React.Component {
 
     render() {
         return (
-            <div className="list list_sortable ">
-                {this.state.list.filter(this.filerList).map((credential, idx) => <ListItem credential={credential} key={idx} />)}
+            <div>
+                <div className="list-menu">
+                    <div style={{width: '70px', height: '30px', backgroundColor: '#d9d9d9', display: 'inline-block'}}>
+                        <input type="checkbox" checked={this.state.allChecked}  onChange={this.checkAll.bind(this)}/> All
+                    </div>
+                    <span>---//Filter goes here</span>
+                    <span> ----//Tags go here</span>
+                    <span> ----//Add button goes here</span>
+
+                </div>
+                <div className="list list_sortable ">
+                    {this.state.list.filter(this.filerList).map((credential, idx) => <ListItem credential={credential} key={idx} />)}
+                </div>
             </div>
+
         );
     }
 }
