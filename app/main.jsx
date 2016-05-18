15c5d510222e97f3dcc279aa67394b42261e6a92
unknown <viktor.shevchenko@KBP1-LHP-F54608.synapse.com>
2016-05-18 16:40:43 +0300

    some clean-up

--- app/main.jsx
+++ app/main.jsx
@@ -16,6 +16,14 @@ let store = createStore(reducers, {}, compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+if (module.hot) {
+  // Enable Webpack hot module replacement for reducers
+  module.hot.accept('./reducers', () => {
+    const nextReducer = require('./reducers');
+    store.replaceReducer(nextReducer);
+  });
+}
+
 ReactDOM.render(
     /*<Router history={browserHistory}>
         <Route path="/" component={App}>
